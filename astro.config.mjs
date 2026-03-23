1a1fdb5e1aefff229434a118bd8af044ebdd4c43
bipolab-eu <bentagai@bipolab.eu>
2026-03-23 11:00:46 +0000

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: true
+    }
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
+});
